142071986a8fff616b8205bf89b3552f6bf5208f
Aquilino Pinto <aquilinopintoa@gmail.com>
2019-01-24 01:22:26 +0100

    CRM-803 plugin integration with CRM

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 // THIS FILE IS EXEC INTO ACTIVE PAGE
 var CRMbaseUrl = 'https://crm.atedev.co.uk';
 var injected = false;
+token = null;
 
 // COMUNICATION WITH BACKGROUND 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
@@ -33,6 +34,12 @@ function init(open) {
             .addEventListener('click', closeSidebar);
         document.querySelector('#submit-form-data')
             .addEventListener('click', submitFormData);
+        document.querySelector('#submit-form-login')
+            .addEventListener('click', login);
+
+        if (!token) {
+            showFormLogin();
+        }
 
         if (open) {
             setTimeout(toggleSidebar);
@@ -60,5 +67,89 @@ function closeSidebar() {
 
 // CRM FORM DATA CONTROL
 function submitFormData () {
-    alert('data sended')
+    event.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", CRMbaseUrl + "/api/suppliers?page=1&perPage=1");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("authorization-app", token);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            switch (xhr.status) {
+                case 200:
+                    const response = JSON.parse(xhr.response)
+                    console.log(response)
+                    break;
+                case 400:
+                    // showError(
+                    //    'That doesn\'t seem right',
+                    //    'Please check your username/ password combination.'
+                    // );
+                    alert('error');
+                    break;
+                case 500:
+                default:
+                    alert('error');
+                    // showError(
+                    //    'Whoops, something went wrong!',
+                    //    'Please try again in a minute.'
+                    // );
+                    break;
+            }
+        }
+    };
+    xhr.send();
+}
+
+// CRM FORM LOGIN CONTROL
+function showFormLogin () {
+    document.getElementById('body')
+        .classList.add('show-login');
+}
+
+function hideFormLogin () {
+    document.getElementById('body')
+        .classList.remove('show-login');
+}
+
+function login () {
+    event.preventDefault();
+    const email = document.getElementById('crm-user-email-field').value
+    const password = document.getElementById('crm-user-password-field').value
+
+    if (!email || !password) {
+        alert('Email and password fields are required!')
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", CRMbaseUrl + "/auth/login");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            switch (xhr.status) {
+                case 200:
+                    const response = JSON.parse(xhr.response)
+                    token = response.token
+                    hideFormLogin()
+                    break;
+                case 400:
+                    // showError(
+                    //    'That doesn\'t seem right',
+                    //    'Please check your username/ password combination.'
+                    // );
+                    alert('error');
+                    break;
+                case 500:
+                default:
+                    alert('error');
+                    // showError(
+                    //    'Whoops, something went wrong!',
+                    //    'Please try again in a minute.'
+                    // );
+                    break;
+            }
+        }
+    };
+    xhr.send(JSON.stringify({email: email, password: password}));
 }
